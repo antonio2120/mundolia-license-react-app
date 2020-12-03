778b3816d152035ec439386606b4bc1a2187833f
Jorge Alejandro Delgado Díaz <jorgeadelgadod@gmail.com>
2020-12-03 13:01:56 -0600

    loading view for login phpFox

--- src/app/main/loginPhpFox/LoginPhpFox.js
+++ src/app/main/loginPhpFox/LoginPhpFox.js
@@ -7,6 +7,7 @@ import Typography from '@material-ui/core/Typography';
 import clsx from 'clsx';
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import axios from "axios";
 import {showMessage} from "../../store/fuse/messageSlice";
 import CircularProgress from '@material-ui/core/CircularProgress';
@@ -17,14 +18,24 @@ const useStyles = makeStyles(theme => ({
 		backgroundImage: "url(assets/images/login/bg_alumnos.png)",backgroundSize:"cover",position:"relative",height:"90%",backgroundSize:"cover",
 		color: theme.palette.primary.contrastText
 	},
-	image_overlay_alumnos:{position:"absolute",left:0,right:0,top:0,bottom:0,backgroundColor:'rgba(148,88,183,0.8)',},
-	root: {
-		background: `linear-gradient(to left, ${theme.palette.primary.dark} 0%, ${darken(
-			theme.palette.primary.dark,
-			0.5
-		)} 100%)`,
+	root_maestros: {
+		backgroundImage: "url(assets/images/login/bg_maestros.png)",backgroundSize:"cover",position:"relative",height:"90%",backgroundSize:"cover",
 		color: theme.palette.primary.contrastText
 	},
+	root_padres: {
+		backgroundImage: "url(assets/images/login/bg_padres.png)",backgroundSize:"cover",position:"relative",height:"90%",backgroundSize:"cover",
+		color: theme.palette.primary.contrastText
+	},
+	root_escuelas: {
+		backgroundImage: "url(assets/images/login/bg_escuelas.png)",backgroundSize:"cover",position:"relative",height:"90%",backgroundSize:"cover",
+		color: theme.palette.primary.contrastText
+	},
+
+	image_overlay_alumnos:{position:"absolute",left:0,right:0,top:0,bottom:0,backgroundColor:'rgba(148,88,183,0.8)',},
+	image_overlay_maestros:{position:"absolute",left:0,right:0,top:0,bottom:0,backgroundColor:'rgb(208,66,55,0.7)',},
+	image_overlay_padres:{position:"absolute",left:0,right:0,top:0,bottom:0,backgroundColor:'rgba(231,162,63,0.7)',},
+	image_overlay_escuelas:{position:"absolute",left:0,right:0,top:0,bottom:0,backgroundColor:'rgba(34,79,245,0.7)',},
+	
 	leftSection: {},
 	rightSection: {
 		background: `linear-gradient(to right, ${theme.palette.primary.dark} 0%, ${darken(
@@ -54,13 +65,17 @@ function getUrl(){
 function LoginPhpFox() {
 	const classes = useStyles();
 	getUrl();
+	const dispatch = useDispatch();
+	const userType = useSelector(({ auth }) => auth.user.role);
+	console.log(userType)
+
 	return (
 		<div
-		className={clsx(classes.root_alumnos,
+		className={clsx((userType == "admin" || userType == "alumno") ? classes.root_alumnos : userType == "maestro" ? classes.root_maestros : userType === "padres" ? classes.root_padres :classes.root_escuelas,
 			'flex flex-col flex-auto items-center justify-center flex-shrink-0 p-16 md:p-24'
 		)}
 	>
-		<div className={clsx(classes.image_overlay_alumnos)}/>
+		<div className={clsx((userType == "admin" || userType == "alumno") ? classes.image_overlay_alumnos : userType == "maestro" ? classes.image_overlay_maestros : userType === "padres" ? classes.image_overlay_padres :classes.image_overlay_escuelas)}/>
 			<FuseAnimate animation="transition.expandIn">
 				<div className={clsx("flex w-full max-w-400 md:max-w-3xl rounded-12 overflow-hidden justify-center")}>
 					
